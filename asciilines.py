ea7c1a07190d8693518ee3d462498d93588d686a
Carissa Allen <carissawood@outlook.com>
2019-06-25 16:23:06 -0700

    Add initial python file with comments

---
+++ asciilines.py
@@ -0,0 +1,8 @@
+# Read .tvg file.
+# First line contains the number of rows and columns for text to output.
+# Original grid should contain dots.
+# 1: character to render (overwrite dot).
+# 2: row position to start (0 based).
+# 3: column position to start (0 based).
+# 4: h for horizontal line, v for vertical line.
+# 5: length of the line ( 0 < line <= column, row)
